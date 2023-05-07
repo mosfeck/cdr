9fd33a79f997cec8958c3d8a2b682452caf72789
mosfeck <mosfeck@office.bdcom.com>
2023-05-07 15:35:48 +0600

    pagination design is ready

--- missed_call.php
+++ missed_call.php
@@ -149,10 +149,10 @@ $results = $conn->query($sqlSelect);
                         <?php // if($page_no > 1){ echo "<li><a href='?page_no=1'>First Page</a></li>"; } 
                         ?>
 
-                        <li <?php if ($page_no <= 1) {
+                        <li class="page-item <?php if ($page_no <= 1) {
                                 echo "class='disabled'";
-                            } ?>>
-                            <a <?php if ($page_no > 1) {
+                            } ?>">
+                            <a class="page-link" <?php if ($page_no > 1) {
                                     echo "href='?page_no=$previous_page'";
                                 } ?>>Previous</a>
                         </li>
@@ -161,9 +161,9 @@ $results = $conn->query($sqlSelect);
                         if ($total_no_of_pages <= 10) {
                             for ($counter = 1; $counter <= $total_no_of_pages; $counter++) {
                                 if ($counter == $page_no) {
-                                    echo "<li class='active'><a>$counter</a></li>";
+                                    echo "<li class='page-link active'><a>$counter</a></li>";
                                 } else {
-                                    echo "<li><a href='?page_no=$counter'>$counter</a></li>";
+                                    echo "<li ><a class='page-link' href='?page_no=$counter'>$counter</a></li>";
                                 }
                             }
                         } elseif ($total_no_of_pages > 10) {
@@ -171,38 +171,38 @@ $results = $conn->query($sqlSelect);
                             if ($page_no <= 4) {
                                 for ($counter = 1; $counter < 8; $counter++) {
                                     if ($counter == $page_no) {
-                                        echo "<li class='active'><a>$counter</a></li>";
+                                        echo "<li class='page-link active'><a>$counter</a></li>";
                                     } else {
-                                        echo "<li><a href='?page_no=$counter'>$counter</a></li>";
+                                        echo "<li><a class='page-link' href='?page_no=$counter'>$counter</a></li>";
                                     }
                                 }
-                                echo "<li><a>...</a></li>";
-                                echo "<li><a href='?page_no=$second_last'>$second_last</a></li>";
-                                echo "<li><a href='?page_no=$total_no_of_pages'>$total_no_of_pages</a></li>";
+                                echo "<li><a class='page-link' >...</a></li>";
+                                echo "<li><a  class='page-link' href='?page_no=$second_last'>$second_last</a></li>";
+                                echo "<li><a  class='page-link' href='?page_no=$total_no_of_pages'>$total_no_of_pages</a></li>";
                             } elseif ($page_no > 4 && $page_no < $total_no_of_pages - 4) {
-                                echo "<li><a href='?page_no=1'>1</a></li>";
-                                echo "<li><a href='?page_no=2'>2</a></li>";
-                                echo "<li><a>...</a></li>";
+                                echo "<li><a  class='page-link' href='?page_no=1'>1</a></li>";
+                                echo "<li><a  class='page-link' href='?page_no=2'>2</a></li>";
+                                echo "<li><a class='page-link' >...</a></li>";
                                 for ($counter = $page_no - $adjacents; $counter <= $page_no + $adjacents; $counter++) {
                                     if ($counter == $page_no) {
-                                        echo "<li class='active'><a>$counter</a></li>";
+                                        echo "<li class='page-link active'><a>$counter</a></li>";
                                     } else {
-                                        echo "<li><a href='?page_no=$counter'>$counter</a></li>";
+                                        echo "<li><a  class='page-link' href='?page_no=$counter'>$counter</a></li>";
                                     }
                                 }
-                                echo "<li><a>...</a></li>";
-                                echo "<li><a href='?page_no=$second_last'>$second_last</a></li>";
-                                echo "<li><a href='?page_no=$total_no_of_pages'>$total_no_of_pages</a></li>";
+                                echo "<li><a class='page-link' >...</a></li>";
+                                echo "<li><a class='page-link'  href='?page_no=$second_last'>$second_last</a></li>";
+                                echo "<li><a  class='page-link' href='?page_no=$total_no_of_pages'>$total_no_of_pages</a></li>";
                             } else {
-                                echo "<li><a href='?page_no=1'>1</a></li>";
-                                echo "<li><a href='?page_no=2'>2</a></li>";
-                                echo "<li><a>...</a></li>";
+                                echo "<li><a class='page-link'  href='?page_no=1'>1</a></li>";
+                                echo "<li><a class='page-link'  href='?page_no=2'>2</a></li>";
+                                echo "<li><a class='page-link' >...</a></li>";
 
                                 for ($counter = $total_no_of_pages - 6; $counter <= $total_no_of_pages; $counter++) {
                                     if ($counter == $page_no) {
-                                        echo "<li class='active'><a>$counter</a></li>";
+                                        echo "<li class='page-link active'><a>$counter</a></li>";
                                     } else {
-                                        echo "<li><a href='?page_no=$counter'>$counter</a></li>";
+                                        echo "<li><a  class='page-link' href='?page_no=$counter'>$counter</a></li>";
                                     }
                                 }
                             }
@@ -212,12 +212,12 @@ $results = $conn->query($sqlSelect);
                         <li <?php if ($page_no >= $total_no_of_pages) {
                                 echo "class='disabled'";
                             } ?>>
-                            <a <?php if ($page_no < $total_no_of_pages) {
+                            <a class="page-link" <?php if ($page_no < $total_no_of_pages) {
                                     echo "href='?page_no=$next_page'";
                                 } ?>>Next</a>
                         </li>
                         <?php if ($page_no < $total_no_of_pages) {
-                            echo "<li><a href='?page_no=$total_no_of_pages'>Last &rsaquo;&rsaquo;</a></li>";
+                            echo "<li><a  class='page-link' href='?page_no=$total_no_of_pages'>Last &rsaquo;&rsaquo;</a></li>";
                         } ?>
                     </ul>
                 </nav>
